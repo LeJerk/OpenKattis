c3305e9e3705986429f9ffc15af6bf42996c23a5
fknjerkz <jerker.sandsten@gmail.com>
2017-03-02 11:32:14 +0100

    Solved

--- OpenKattisTasks/solvedforcarrots/SFC.java
+++ OpenKattisTasks/solvedforcarrots/SFC.java
@@ -1,10 +1,19 @@
 package solvedforcarrots;
 
-public class SFC {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+public class SFC 
+{
+	public static void main(String[] args) throws Exception 
+	{
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(bfr.readLine());
+		bfr.close();
+		
+		int n = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		System.out.println(p);
 	}
-
 }
