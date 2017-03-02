410d646261b409255db32c659e9047d538ac63f8
fknjerkz <jerker.sandsten@gmail.com>
2017-03-02 12:22:09 +0100

    TakeTwoStones

---
+++ OpenKattisTasks/taketwostones/Stones.java
@@ -0,0 +1,14 @@
+package taketwostones;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Stones 
+{
+	public static void main(String[] args) throws Exception
+	{
+		BufferedReader bfr = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(bfr.readLine());
+		System.out.println(n % 2 == 1 ? "Alice" : "Bob");	
+	}
+}
